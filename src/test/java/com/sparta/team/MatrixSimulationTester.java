9c33b975854bbea8300d5f04dcd6fc87a2b75bd7
Colette Shaw <CShaw@spartaglobal.com>
2020-03-25 14:37:11 +0000

    Add two tests

---
+++ src/test/java/com/sparta/team/MatrixSimulationTester.java
@@ -0,0 +1,48 @@
+package com.sparta.team;
+
+import com.sparta.team.manager.MatrixSimulation;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixSimulationTester {
+
+    MatrixSimulation matrixSimulation = new MatrixSimulation();
+
+    @BeforeEach
+    public void setup(){
+        matrixSimulation.startSimulation(10);
+
+    }
+
+    @Test
+    void update() {
+//        assertEquals()
+    }
+
+    @Test
+    void getRabbitCouples() {
+        MatrixSimulation matrixSimulation2 = new MatrixSimulation();
+        matrixSimulation2.startSimulation(3);
+        assertEquals(matrixSimulation2.getRabbitCouples(), 1);
+    }
+
+    @Test
+    void ageRabbits() {
+        assertEquals(matrixSimulation.getFemaleRabbitsByAge().get(10), 1);
+    }
+
+    @Test
+    void generateNewGeneration() {
+    }
+
+    @Test
+    void removeDeadRabbits() {
+    }
+
+    @Test
+    void updateAliveRabbits() {
+    }
+}
