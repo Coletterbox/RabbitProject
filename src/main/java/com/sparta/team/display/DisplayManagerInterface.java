64b9e0cc4c5bfd0bce5916aace0c9b50e8210676
Tolga Komur <tkomur@spartaglobal.com>
2020-03-26 15:06:43 +0000

    Added Extension for foxes for Display Manager

--- src/main/java/com/sparta/team/display/DisplayManagerInterface.java
+++ src/main/java/com/sparta/team/display/DisplayManagerInterface.java
@@ -3,13 +3,7 @@ package com.sparta.team.display;
 public interface DisplayManagerInterface {
 
     void displayTimeElapsed(int time);
-    void displayMaleRabbitsLived(long rabbits);
-    void displayFemaleRabbitsLived(long rabbits);
-    void displayRabbitsLived(long maleRabbits, long femaleRabbits);
-
-    void displayMaleRabbitsAlive(long rabbits);
-    void displayFemaleRabbitsAlive(long rabbits);
-    void displayRabbitsAlive(long maleRabbits, long femaleRabbits);
-
-
+    void displayRabbitsLived(String animalType,long maleRabbits, long femaleRabbits);
+    void displayRabbitsAlive(String animalType,long maleRabbits, long femaleRabbits);
+    void writerClose();
 }
